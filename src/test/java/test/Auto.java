aaf55e4e32cf2817c01fa876ba41a6bc5c44f26e
Sabelsl <141966602+Sabelsl@users.noreply.github.com>
2023-08-21 20:27:40 -0500

    :alarm_clock:

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -7,16 +7,44 @@ public class Auto {
 	String marca;
 	Motor motor;
 	int registro;
-
-	public static int cantidadCreado;
-	int cantidadAsientos;
+	static int cantidadCreados;
 	
-		public static void main (String[]args) {
-			
+
+	int cantidadAsientos()
+	{
+		int numeroAsientos = 0;
+		for (int i =0; i< asientos.length; i++)
+		{
+			if (asientos[i]!= null)
+			{
+				numeroAsientos++;
+			}
+		}
+		return numeroAsientos;
+		
+		
 		}
 			
-	
-	
-	
-
+		String verificarIntegridad()
+		{
+			if(this.registro == this.motor.registro)
+			{
+				for(int i = 0;i < asientos.length;i++)
+					
+				{
+					if (asientos[i]!= null)
+					{
+							if(asientos[i].registro != registro)
+							{
+						return "Las piezas no son orginales";
+						
+					}
+				}
+			}
+			return "Auto original";
+		}else {
+				return "Las piezas no son originales";
+		}
+	}
 }
+		
